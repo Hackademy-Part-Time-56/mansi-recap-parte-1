3461b8539f5499e17e948784905fda9e8824b8f6
francesco mansi <f.mansi@socialwebsolutions.it>
2025-07-15 20:43:32 +0200

    aggiunto livewire con create

---
+++ app/Livewire/CreateProduct.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Product;
+use Livewire\Attributes\Validate;
+use Livewire\Component;
+
+class CreateProduct extends Component
+{
+    #[Validate]
+    public $title;
+    #[Validate]
+    public $description;
+    #[Validate]
+    public $quantity;
+    public $image;
+
+    public function render()
+    {
+        return view('livewire.create-product');
+    }
+    protected function rules()
+    {
+        return [
+            'title' => ['required', 'min:5'],
+            'quantity' => ['required', 'integer'],
+            'description' => ['required', 'min:10'],
+        ];
+    }
+    public function save()
+    {
+        $this->validate();
+        Product::create([
+            'title' => $this->title,
+            'description' => $this->description,
+            'quantity' => $this->quantity,
+        ]);
+
+        $this->reset();
+
+        session()->flash('success', 'Prodotto creato con successo.');
+    }
+}
---
+++ resources/views/livewire/create-product.blade.php
@@ -0,0 +1,33 @@
+<div>
+    @if (session('success'))
+        Prodotto inserito correttamente
+    @endif
+    <form wire:submit="save">
+        <div class="form-floating mb-3">
+            <input class="form-control" id="title" value name="title" type="text" wire:model.live="title">
+            <label for="name">Titolo</label>
+            @error('title')
+                {{ $message }}
+            @enderror
+        </div>
+        <div class="form-floating mb-3">
+            <input class="form-control" id="quantity" value name="quantity" type="text" wire:model.live="quantity">
+            <label for="name">Quantita</label>
+            @error('quantity')
+                {{ $message }}
+            @enderror
+        </div>
+        <div class="form-floating mb-3">
+            <textarea style="height: 10rem" wire:model.live="description" class="form-control" name="description" id=""
+                cols="30" rows="10"></textarea>
+            <label for="name">descrizione</label>
+            @error('description')
+                {{ $message }}
+            @enderror
+        </div>
+
+        <div class="d-grid">
+            <button class="btn btn-primary btn-lg" type="submit">Salva</button>
+        </div>
+    </form>
+</div>
