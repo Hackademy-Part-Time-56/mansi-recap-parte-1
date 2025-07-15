2a0619fd7d940ef670bd820238229e4240095abd
francesco mansi <f.mansi@socialwebsolutions.it>
2025-07-15 18:58:25 +0200

    aggiunto stringa test

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(ContactController::class)->group(function () {
     Route::get('/contatti', 'form')->name('contact.form');
+    //ciao
     Route::post('/invia-messaggio', 'send')->name('contact.send');
 });
 
